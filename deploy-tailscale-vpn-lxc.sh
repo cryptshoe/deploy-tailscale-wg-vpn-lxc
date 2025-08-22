e20786cddc9110f1b153f5e2e729467e67600484
cryptshoe <82201739+cryptshoe@users.noreply.github.com>
2025-08-22 21:09:19 +1000

    Update deploy-tailscale-vpn-lxc.sh

--- deploy-tailscale-vpn-lxc.sh
+++ deploy-tailscale-vpn-lxc.sh
@@ -104,10 +104,6 @@ echo "ERROR: VPN config file /etc/wireguard/vpn.conf missing after retries"
 exit 1
 '
 
-# Add Tailscale package signing key and repo (adjust for your OS/Version)
-ID=$(grep ^ID= /etc/os-release | cut -d= -f2 | tr -d '"')
-VER=$(grep ^VERSION_CODENAME= /etc/os-release | cut -d= -f2 | tr -d '"')
-
 msg_info "Generating setup.sh inside container..."
 
 pct exec $CTID -- bash -c "cat > /root/setup.sh" <<EOF
@@ -133,8 +129,8 @@ export LC_ALL=en_US.UTF-8
 echo "Restarting networking service before adding Tailscale repo..."
 systemctl restart networking
 
-curl -fsSL https://pkgs.tailscale.com/stable/"${ID}\"/"${VER}\".noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/tailscale-archive-keyring.gpg] https://pkgs.tailscale.com/stable/"${ID}\" "${VER}\" main" | tee /etc/apt/sources.list.d/tailscale.list
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/tailscale-archive-keyring.gpg] https://pkgs.tailscale.com/stable/debian bookworm main" | tee /etc/apt/sources.list.d/tailscale.list
 
 apt-get update -qq
 apt-get install -y tailscale
