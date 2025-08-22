ea06e906d1dc20c03ccc1c8d8e2007c0cbc6b65b
cryptshoe <82201739+cryptshoe@users.noreply.github.com>
2025-08-22 21:31:42 +1000

    Update deploy-tailscale-vpn-lxc.sh

--- deploy-tailscale-vpn-lxc.sh
+++ deploy-tailscale-vpn-lxc.sh
@@ -139,10 +139,10 @@ apt-get update -qq
 apt-get install -y tailscale
 
 # Enable IP forwarding
-cat <<EOF > /etc/sysctl.d/99-tailscale.conf
+cat <<EOT >/etc/sysctl.d/99-tailscale.conf
 net.ipv4.ip_forward=1
 net.ipv6.conf.all.forwarding=1
-EOF
+EOT
 
 sysctl --system
 
